c3e3c608ef4b4245c6f511eff64d4c678b64f97a
nezumisama <narf@bbis.us>
2017-09-11 19:43:18 +0200

    Updated for new Youtube layout, added canceling of interval after success, code cleanup.

--- mpv_play/mpv_play.user.js
+++ mpv_play/mpv_play.user.js
@@ -3,12 +3,12 @@
 // @namespace   nez
 // @description Add an link (eject symbol) on youtube video pages opening the video in MPV (or similar).
 // @include     /^https?://(?:www)?\.youtube\.com/
-// @version     0.2
+// @version     0.3
 // @grant       GM_addStyle
 // ==/UserScript==
 
 // Add styling rules for the element.
-GM_addStyle('#mpvlink { font-size: 50%; vertical-align: middle; } #mpvlink:hover { color: #77aadd; text-decoration: none; }');
+GM_addStyle('#mpvlink { color: inherit; text-decoration: none; } #mpvlink:hover { color: #77aadd; text-decoration: none; }');
 
 // Create the link.
 var mpvlink = document.createElement('a');
@@ -16,19 +16,23 @@ mpvlink.id = 'mpvlink';
 mpvlink.title = 'Play the video in MPV.';
 mpvlink.text = '⏏';
 // Pause the on-page video when clicked.
-mpvlink.onclick = function(e) { document.getElementsByTagName('video')[0].pause(); }
+var onclick = function(e) {
+  document.getElementsByTagName('video')[0].pause();
+}
+mpvlink.addEventListener('click', onclick);
 
 var add_link_if_possible = function () {
-  mpv_lnk = document.getElementById('mpvlink');
-  tit = document.getElementById('eow-title');
+  var mpv_lnk = document.getElementById('mpvlink');
+  var tit = document.querySelector('h1.title');
   // If the title element exists and the link doesn't,
-  // add the link in title.
+  // add the link in the title.
   if(!mpv_lnk && tit) {
     mpvlink.href = 'mpv://' + document.location.href;
-    tit.parentElement.appendChild(mpvlink);
+    tit.appendChild(mpvlink);
+    window.clearInterval(interval_id);
   }
 }
 
 add_link_if_possible();
 // Repeatedly try adding.
-window.setInterval(add_link_if_possible, 2000);
+var interval_id = window.setInterval(add_link_if_possible, 2000);
